90ea59f2dbe6d94d164640fb7b5ec3eb8907cb83
Joao Claudio <jcdias1141@gmail.com>
2024-08-18 21:14:23 -0300

    Adiciona arquivos ao repositório

---
+++ js/tipos-de-dados.js
@@ -0,0 +1,18 @@
+let infoCarro = [
+    {
+        nome: "Ferrari",
+        marca: "Ferrari",
+        ano: 2020,
+        modelo: "F40",
+        cor: "Preto",
+        tipo: "Sedan",
+        preço: 100000,
+        imagem: "https://i.pinimg.com/originals/b1/f1/c7/b1f1c7f6c4a6f3e6d3f5e4e7a5a8a7d.jpg"
+    }
+]
+console.log(infoCarro[0].tipo);
+
+let primeiroNome = "João Claudio";
+let segundoNome = "Dias Carvalho";
+
+console.log(`Nome: ${primeiroNome} ${segundoNome}`);
